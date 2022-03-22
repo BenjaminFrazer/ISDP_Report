181cfbb53b05f907786f081e576f8424e0e2e31c
benjaminf <benjamin.frazer.1997@gmail.com>
2022-03-22 23:40:58 +0000

    Minor aspthetic improvements

---
+++ .auctex-auto/biblio.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "biblio"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "1s"
+    "2s"
+    "3s"
+    "4s"
+    "5s"
+    "6s"
+    "7s"
+    "refLowFrequency"
+    "LevelizedCost"
+    "123"
+    "1234"
+    "12345"
+    "123abc"
+    "123bcd"
+    "123cde"
+    "oecdKeyIndicators"
+    "10.1016/j.finel.2014.02.002"
+    "10.1016/j.ijepes.2021.107273"
+    "10.1109/ispa-bdcloud-socialcom-sustaincom51426.2020.00190"
+    "10.1061/"
+    "10.1177/1356766713481218"
+    "10.2749/101686606778995164"
+    "10.2749/101686606778995119"))
+ :bibtex)
+
